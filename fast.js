9f3990efccc626022fbbefe6ffc417d9456ffe06
Taylor Reece <taylor.reece@prismatic.io>
2021-08-17 14:34:52 -0500

    Added a few comments.

--- fast.js
+++ fast.js
@@ -3,11 +3,11 @@ const fs = require("fs");
 
 const expression = `
 $map($, function($customer) {
-    {
-      "Id": ($customer.id),
-      "Name": ($customer.name)
-    }
-  })
+  {
+    "Id": ($customer.id),
+    "Name": ($customer.name)
+  }
+})
 `;
 
 const jsonataExpression = jsonata(expression);
@@ -34,9 +34,12 @@ inputSizes.forEach((count) => {
   for (var i = 0; i < count; i++) {
     sampleInput.push(sampleObject);
   }
+
+  // Check how long it takes to evaluate the input data:
   const startTime = new Date();
   jsonataExpression.evaluate(sampleInput);
   const endTime = new Date();
+
   console.log(
     `Input of size ${count} took ${endTime - startTime}ms to process.`
   );
--- slow.js
+++ slow.js
@@ -3,17 +3,17 @@ const fs = require("fs");
 
 const expression = `
 $map($, function($customer) {
-    {
-      "Id": ($customer.id),
-      "Name": ($customer.name),
-      "Address": $map($customer.addresses, function($address) {
-        {
+  {
+    "Id": ($customer.id),
+    "Name": ($customer.name),
+    "Address": $map($customer.addresses, function($address) {
+      {
           "Address1": ($address.addressLine1),
           "Address2": ($address.addressLine2)
-        }
-      })
-    }
-  })
+      }
+    })
+  }
+})
 `;
 
 const jsonataExpression = jsonata(expression);
@@ -40,9 +40,12 @@ inputSizes.forEach((count) => {
   for (var i = 0; i < count; i++) {
     sampleInput.push(sampleObject);
   }
+
+  // Check how long it takes to evaluate the input data:
   const startTime = new Date();
   jsonataExpression.evaluate(sampleInput);
   const endTime = new Date();
+
   console.log(
     `Input of size ${count} took ${endTime - startTime}ms to process.`
   );
